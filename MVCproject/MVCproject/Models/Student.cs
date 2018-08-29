11d78833a18536793924720cf080fc401740ec5c
LABINSAPKOTA\labin <loveinspk@gmail.com>
2018-08-29 13:29:35 +0930

    GIt file for home exercise of MVC

---
+++ MVCproject/MVCproject/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCproject.Models
+{
+	public class Student
+	{
+
+		public int StudentID { get; set; }
+		[Required]
+		[StringLength(255)]
+		public string Name { get; set; }
+		public string Age { get; set; }
+		public bool IsFullTimeStudent { get; set; }
+		public campus campus  { get; set; }
+		public EnrolledSubject EnrolledSubject { get; set; }
+
+	}
+}
---
+++ MVCproject/MVCproject/Models/campus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCproject.Models
+{
+	public class campus
+	{
+		public String Casuarina { get; set; }
+		public String Palmerstorm { get; set; }
+	}
+
+	public class EnrolledSubject
+	{
+		public String PRT455 { get; set; }
+		public String PRT456 { get; set; }
+		public String PRT457 { get; set; }
+
+	}
+
+
+
+
+
+}
