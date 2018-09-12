5ea5b4c164cae49a28b7add6f6fcfe64afe1962a
aayushsapkota <96.estd@gmail.com>
2018-09-12 13:24:02 +0930

    initial clean build with gitignore file updated so unneccessary build file is not uploaded

---
+++ Safe-Heaven-2018/Safe-Heaven-2018/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Safe_Heaven_2018.MvcApplication" Language="C#" %>
---
+++ Safe-Heaven-2018/Safe-Heaven-2018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Safe_Heaven_2018.Startup))]
+namespace Safe_Heaven_2018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
