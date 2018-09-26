a4dba7b6e3fdb9574ea235d9e892d358ddc1b6dd
LAPTOP-GLGB6S1T\kumar <dixitkakkar@gmail.com>
2018-09-26 16:51:08 +0930

    new model classes page content and log table

---
+++ Safe-Heaven-2018/Safe-Heaven-2018/Models/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Safe_Heaven_2018.Models
+{
+    public class Log
+    {
+        public int logId { get; set; }
+        public long logtime { get; set; }
+        public string logDecsription { get; set; }
+        [ForeignKey("user")]
+        public int UserId { get; set; }
+    }
+}
---
+++ Safe-Heaven-2018/Safe-Heaven-2018/Models/Page.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Safe_Heaven_2018.Models
+{
+    public class Page
+    {
+        public int pageId { get; set; }
+        public string pageHeading { get; set; }
+        public string pageSubHeading { get; set; }
+        public string pageContent1 { get; set; }
+        public string pageContent2 { get; set; }
+        public string pageContent3 { get; set; }
+        public string pageContent4 { get; set; }
+        public string pageContent5 { get; set; }
+        public string pageHeading1 { get; set; }
+        public string pageHeading2 { get; set; }
+        public string pageHeading3 { get; set; }
+        public string pageHeading4 { get; set; }
+        public string pageHeading5 { get; set; }
+        public string pageHeading6 { get; set; }
+    }
+}
