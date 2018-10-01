78a5f71e2da8e36536918b49e86ca3ede4771182
LAPTOP-GLGB6S1T\kumar <dixitkakkar@gmail.com>
2018-10-01 20:59:54 +0930

    players model added

---
+++ dashBoard/edit files/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Safe_Heaven_2018.Models
+{
+    public class Player
+    {
+        [Key]
+        public int playerId { get; set; }
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public DateTime Date = DateTime.Now;
+
+
+        public virtual ICollection<Score> Scores { get; set; }
+
+
+
+    }
+}
